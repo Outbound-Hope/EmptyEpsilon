563f4e76546c8aaa03e9ba36676ee8fc0bf465e1
Amir Arad <greenshade@gmail.com>
2019-10-26 16:45:44 +0300

    rename jump -> skip

--- src/screenComponents/jumpControls.cpp
+++ src/screenComponents/jumpControls.cpp
@@ -21,7 +21,7 @@ GuiJumpControls::GuiJumpControls(GuiContainer* owner, string id)
     label = new GuiKeyValueDisplay(this, id + "_LABEL", 0.5, "Distance", "10.0");
     label->setTextSize(30)->setPosition(50, -50, ABottomLeft)->setSize(40, GuiElement::GuiSizeMax);
     
-    button = new GuiButton(this, id + "_BUTTON", "Jump", [this]() {
+    button = new GuiButton(this, id + "_BUTTON", "Skip", [this]() {
         my_spaceship->commandJump(slider->getValue());
     });
     button->setPosition(0, 0, ABottomLeft)->setSize(GuiElement::GuiSizeMax, 50);
@@ -35,7 +35,7 @@ void GuiJumpControls::onDraw(sf::RenderTarget& window)
     {
         if (my_spaceship->jump_delay > 0.0)
         {
-            label->setKey("Jump in");
+            label->setKey("Skip in");
             label->setValue(string(int(ceilf(my_spaceship->jump_delay))));
             slider->disable();
             button->disable();
